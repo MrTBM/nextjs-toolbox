e38968c2eb22671ad74268649ba85e07f4762d76
Thomas Markey <84930305+MrTBM@users.noreply.github.com>
2023-03-10 13:32:47 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Fosshost v2</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Next.js Toolbox" />
+        <Header title="Welcome to Fosshost v2" />
         <hr />
         <p className="description">
-          Here's an example of a Netlify Form! When you fill this out, the
-          submissions can be found in the Netlify Admin site.
+          We are in the process of building a new website.  For now please use the below contact us form if you have any questions.
         </p>
         <FeedbackForm />
         <JokeBlock />
